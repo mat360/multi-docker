b6f478f565ba2aba9421f7ed18fb16dc7afb5365
mat360 <43040029+mat360@users.noreply.github.com>
2024-05-08 08:59:38 +0200

    initial commit

---
+++ client/src/App.test.js
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+// On commente ce test car il fait probablement un appel au serveur derrière et ne pourra pas passer en 
+// standalone 
+//  render(<App />);
+//  const linkElement = screen.getByText(/learn react/i);
+//  expect(linkElement).toBeInTheDocument();
+});
